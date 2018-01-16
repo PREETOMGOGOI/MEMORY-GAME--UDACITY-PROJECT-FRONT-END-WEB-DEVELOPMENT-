11c7a346d5913cfd81db79dafeec25cfa03169e8
PREETOM GOGOI(FRONT END WEB DEVELOPER) <31864735+PREETOMGOGOI@users.noreply.github.com>
2018-01-16 15:38:27 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ const matchFound = 0;
 const gameStarted = false;
 const closeCards=[];
 const openCards=[];
-let turn = $('.moves')
 
  	
 
@@ -42,7 +41,7 @@ let moves = 0;
 let totalMoves = 0;
 let secondsCount = 0;
 let timeCount=0;
-
+let timeStart =0;
 
 //Codes for the main game
 
@@ -52,17 +51,21 @@ $('.card').click(function(event) {
 	let value = $(this).attr('id');
 	let value1 = $(this).children('i').attr('id');
  	count +=1;
-
-
-// check matched cards
-	if ($(this).hasClass('match')&&count==1) {
+ 	timeStart+=1;
+ 
+ //Initiate Timer	
+	if (timeStart===1){
+ 		time();
+	}
+    
+		
+	if ($(this).hasClass('match')&&count===1) {
  			while(openCards.length=0){
 			openCards.pop(value);}
 			while(closeCards.length=0){
 			closeCards.pop(value1);}
 			count=0;
-			totalMoves-=1;
-			moves-=17;
+			
  		}
  	
 	if (count===1) {
@@ -72,52 +75,51 @@ $('.card').click(function(event) {
 			closeCards.pop(value1);}
  			openCards.push(value);
  			closeCards.push(value1);
- 			$(this).addClass('open show')
+ 			$(this).addClass('open')
 	}
  	
  	else if (count<=2) {
  			openCards.push(value);
  			closeCards.push(value1);
- 			$(this).addClass('open show');
+ 			$(this).addClass('open');
  			
-
+//Checking for matched cards
  		if ((openCards[0]===openCards[1])&&(closeCards[0]!==closeCards[1])) {
- 				jQuery("#"+closeCards[0]).parent('li').addClass('match');
- 				jQuery("#"+closeCards[1]).parent('li').addClass('match');
+ 				jQuery("#"+closeCards[0]).parent('li').addClass('match show');
+ 				jQuery("#"+closeCards[1]).parent('li').addClass('match show');
+ 				totalMoves+=1;
  				count=0;
- 				
+ 				moves+=1;
  		}	
 		
 		if(closeCards[0]===closeCards[1]) {
-			$(this).addClass('open show')
+			$(this).addClass('open')
  			count=1;
  			openCards.pop(value);
  			closeCards.pop(value);
-			totalMoves-=1;
- 		}
- 		
- 		else if (count===2 && $(this).hasClass('match')) {
+ 		}	
+ 		else if (count===2) {
+ 			if ($(this).hasClass('match')) {
 				count=1;
 				openCards.pop(value);
 				closeCards.pop(value);
-				totalMoves-=1;
-				moves-=17;
-		}	
-
+			}	
+	
 		else{
- 			
+ 			totalMoves+=1;
  			let counter=1;
+ 	// Flipping cards back to hidden position if two cards are not similar	
 				function timeOut(){
 				counter--;
 				if (counter<=0) {
 						clearInterval(loop);
-						jQuery("#"+closeCards[0]).parent('li').removeClass('open show');
- 						jQuery("#"+closeCards[1]).parent('li').removeClass('open show');
+						jQuery("#"+closeCards[0]).parent('li').removeClass('open');
+ 						jQuery("#"+closeCards[1]).parent('li').removeClass('open');
 					}
 					}
 				const loop = setInterval(timeOut,300);}
-
- 	}
+ 		}
+ }
 
  	else if (count>2) {
  		count=1;
@@ -129,7 +131,7 @@ $('.card').click(function(event) {
 		}
  		openCards.push(value);
  		closeCards.push(value1);
- 		$(this).addClass('open show');
+ 		$(this).addClass('open');
  		
     if ($(this).hasClass('match')&&count==1) {
  			while(openCards.length=0){
@@ -141,95 +143,62 @@ $('.card').click(function(event) {
 
  	}
  
+
 //Rating players and ending the game
 
-totalMoves+=1;
-for (var k = 0; k <=cardList.length ; k++) {
-if($(this).hasClass('match')){
-moves+=1;}
-}
-console.log(moves);
-if (totalMoves===1) {
-	time();		
-}
+if (totalMoves!==0){
 
-if (totalMoves/2!==0){
-
-	if (totalMoves/2<=13) {
-		if (moves===136) {
+	if (totalMoves<=13) {
+		if (moves===8) {
 			modalBox.threeStar();
 			let winTime=$('#timer').html();
 			$('#timer').remove();
 			$('.restart').html('<i class="fa fa-repeat"></i><i id="timer">'+winTime+'</i>')
-			$('.moves').html(' Moves-'+Math.floor(totalMoves/2));
-			moves -= 17;
-			totalMoves--;
-			$('.modal').addClass('show');
+			$('.moves').html(' Moves-'+(totalMoves));
+			moves =0;
+			totalMoves=0;
 		} 
 		else {
-			$('.moves').html(' Moves-'+Math.floor(totalMoves/2));
+			$('.moves').html(' Moves-'+(totalMoves));
 		}
 	}
 
-	else if ((totalMoves/2>13 && totalMoves/2<=18)) {
-		if (moves===136) {
+	else if ((totalMoves>13 && totalMoves<=20)) {
+		if (moves===8) {
 			modalBox.twoStar();		
 			let winTime=$('#timer').html();
 			$('#timer').remove();
 			$('.restart').html('<i class="fa fa-repeat"></i><i id="timer">'+winTime+'</i>')
-			$('.moves').html(' Moves-'+Math.floor(totalMoves/2));
-			moves-=17;
-			totalMoves--;				
+			$('.moves').html(' Moves-'+(totalMoves));
+			moves=0
+			totalMoves=0;				
 		}
-		if (timeCount===3) {
-			$('.stars').html('');
-		}		
-		else if (timeCount===2) {
+		if (timeCount===2) {
 			$('.stars').html('<li><i class="fa fa-star"></i></li>');
 		}
 		else{
 			$('.stars').html('<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>');
-			$('.moves').html(' Moves-'+Math.floor(totalMoves/2));
+			$('.moves').html(' Moves-'+(totalMoves));
 		}
 	}	
 
 
-	else if ((totalMoves/2>18 && totalMoves/2<=24)) {
-		if (moves===136) {
+	else if (totalMoves>20) {
+		if (moves===8) {
 			modalBox.oneStar();
 			let winTime=$('#timer').html();
 			$('#timer').remove();
 			$('.restart').html('<i class="fa fa-repeat"></i><i id="timer">'+winTime+'</i>')
-			$('.moves').html(' Moves-'+Math.floor(totalMoves/2));
-			moves-=17
-			totalMoves--;			
-		}
-			
-		if (timeCount===3) {
-			$('.stars').html('');
+			$('.moves').html(' Moves-'+(totalMoves));
+			moves=0
+			totalMoves=0;			
 		}
 		else{	
 			$('.stars').html('<li><i class="fa fa-star"></i></li>');
-			$('.moves').html(' Moves-'+Math.floor(totalMoves/2));
+			$('.moves').html(' Moves-'+Math.floor(totalMoves));
 		}
 	}
 
-
-	else if ((totalMoves/2>24)) {
-		if (moves===136) {
-			modalBox.noStar();
-			let winTime=$('#timer').html();
-			$('#timer').remove();
-			$('.restart').html('<i class="fa fa-repeat"></i><i id="timer">'+winTime+'</i>')
-			$('.moves').html(' Moves-'+Math.floor(totalMoves/2));
-			moves-=17
-			totalMoves--;	
-		}
-		else{
-			$('.moves').html('Moves-'+Math.floor(totalMoves/2));
-			$('.stars').html('');
-		}
-	}
 }
 })
 }
@@ -242,6 +211,7 @@ if (totalMoves/2!==0){
 	totalMoves=0;
 	moves=0;
 	timeCount=0;
+	timeStart=0;
 	$('.restart').html('<i class="fa fa-repeat"></i><i id="timer">00:00</id>');
 	$('.stars').html('<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i>');
 	$('.moves').html('Moves-0');
@@ -253,11 +223,14 @@ $('.stars').html('<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"><
 
 
 
-//SET TIMER
+/*SET TIMER*/
 
+//Timer to be displayed
 function convertSeconds(s){
 	let mins = Math.floor(s/60);
 	let seconds = s%60;
+	let minsDisplay= mins;
+	let secondsDisplay =seconds;
 	if (mins<10) {
 		mins= '0'+mins; 
 	}
@@ -267,27 +240,41 @@ function convertSeconds(s){
 	return mins+":"+seconds;
 }
 
+//Display the time taken to win the game
+function winTime(t){
+	let mins = Math.floor(t/60);
+	let seconds = t%60;
+	if (mins=0) {
+		return seconds+'sec';
+	}
+	else {
+		return mins+'min '+seconds+'sec'
+	}
+}
 
 //Counting seconds and assigning star ratings.
 
 function time(){
 
 var timer = $('#timer');
+let dummyTimer = $('#dummy_timer')
 timer.html(convertSeconds(0));
+dummyTimer.html(winTime(0));
+
 
 function timeUp(){
-if (totalMoves===0&&moves===0) {
+if (timeStart==0) {
 	clearInterval(loopA);
 	secondsCount=0;
 }
 else{
 	secondsCount++;
 	timer.html(convertSeconds(secondsCount));
-	
+	dummyTimer.html(winTime(secondsCount));
 	let displays= $('#timer').html();
 	
 	if (displays==='00:22') {
-		if (totalMoves/2>18&&totalMoves/2<=24) {
+		if (totalMoves>20) {
 			$('.stars').html('<li><i class="fa fa-star"></i></li>');
 		}
 		else{
@@ -295,20 +282,10 @@ else{
 			timeCount=1;		
 		}
 	}
-	else if (displays==='00:34') {
-			if (totalMoves/2>24) {
-		$('.stars').html('');
+	else if (displays==='00:34') {	
+		$('.stars').html('<li><i class="fa fa-star"></i></li>');		
 		}
-			else{
-		$('.stars').html('<li><i class="fa fa-star"></i></li>');
-		timeCount=2;	
-			}
-		}
-	else if (displays==='00:42') {
-		$('.stars').html('');
-		timeCount=3
-		}
-}
+	}
 }
 const loopA = setInterval(timeUp,1000);
 }
@@ -316,21 +293,16 @@ const loopA = setInterval(timeUp,1000);
 //Modal to diplay the final result
 
 const modalBox={
-threeStar : function(){
-$('.modal').html('<p><h1>INVINCIBLE!!!</h1></p><p><h2>'+$('.moves').html()+'</h2></p><p><h2>Rating: 3 Stars</h2></p><p><h2>Time Taken--'+$('#timer').html()+'</h2></p><p><h3>Do you Want to play again..???<h3></p><i"clicked">Click here</i');
+threeStar : function(){	
+$('.modal').html('<p><h1>INVINCIBLE!!!</h1></p><p><h2>'+$('.moves').html()+'</h2></p><p><h2>Rating: 3 Stars</h2></p><p><h2>Time Taken--'+$('#dummy_timer').html()+'</h2></p><p><h3>Do you Want to play again..???<h3></p><i"clicked">Click here</i');
 $('.modal').addClass('show');
 },
 twoStar : function(){
-$('.modal').html('<p><h1>SMOOTH!!</h1></p><p><h2>'+$('.moves').html()+'</h2></p><p><h2>Rating: 2 Stars</h2></p><p><h2>Time Taken--'+$('#timer').html()+'</h2></p><p><h3>Do you Want to play again..???<h3></p><i"clicked">Click here</i');
+$('.modal').html('<p><h1>SMOOTH!!</h1></p><p><h2>'+$('.moves').html()+'</h2></p><p><h2>Rating: 2 Stars</h2></p><p><h2>Time Taken--'+$('#dummy_timer').html()+'</h2></p><p><h3>Do you Want to play again..???<h3></p><i"clicked">Click here</i');
 $('.modal').addClass('show');
 },
 oneStar : function(){
-$('.modal').html('<p><h1>SWEET!</h1></p><p><h2>'+$('.moves').html()+'</h2></p><p><h2>Rating: 1 Star</h2></p><p><h2>Time Taken--'+$('#timer').html()+'</h2></p><p><h3>Do you Want to play again..???<h3></p><i"clicked">Click here</i');
-$('.modal').addClass('show');
-},
-noStar : function(){
-$('.modal').html('<p><h1>GREAT</h1></p><p><h2>'+$('.moves').html()+'</h2></p><p><h2>Time Taken--'+$('#timer').html()+'</h2></p><p><h3>Do you Want to play again..???<h3></p><i"clicked">Click here</i');
+$('.modal').html('<p><h1>SWEET!</h1></p><p><h2>'+$('.moves').html()+'</h2></p><p><h2>Rating: 1 Star</h2></p><p><h2>Time Taken--'+$('#dummy_timer').html()+'</h2></p><p><h3>Do you Want to play again..???<h3></p><i"clicked">Click here</i');
 $('.modal').addClass('show');
 }
-
 }
